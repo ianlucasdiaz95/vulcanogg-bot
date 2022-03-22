48622a74cba015e3204a1f317578d0800655eed0
ian-clica <ian@clica.com.ar>
2022-03-22 16:56:21 -0300

    asd

--- controllers/webhook.controller.js
+++ controllers/webhook.controller.js
@@ -8,42 +8,11 @@ const webhookEn = (req = request, res = response) => {
      res.json({
         msg: 'Endpoint Deshabilitado',
     });
-    
+
 }
-
-const basePost = (req, res = response) => {
-
-     res.json({
-        msg: 'Endpoint Deshabilitado',
-    });
-}
-
-const basePut = (req, res = response) => {
-
-     res.json({
-        msg: 'Endpoint Deshabilitado',
-    });
-}
-
-const basePatch = (req, res = response) => {
-     res.json({
-        msg: 'Endpoint Deshabilitado',
-    });
-}
-
-const baseDelete = (req, res = response) => {
-     res.json({
-        msg: 'Endpoint Deshabilitado',
-    });
-}
-
 
 
 
 module.exports = {
-    baseGet,
-    basePost,
-    basePut,
-    basePatch,
-    baseDelete,
+    webhookEn
 }
