b7c19936a2add25f09e218e4fd59a46594630f2b
ian-clica <ian@clica.com.ar>
2022-03-22 15:18:27 -0300

    asd

--- bots/telegram/bot.js
+++ bots/telegram/bot.js
@@ -102,7 +102,7 @@ class Bot {
     }
 
     async welcomeMessage(){
-        this.bot.on('message', (msg) => {
+        this.bot.on('new_chat_members', (msg) => {
     
             var chatId = msg.chat.id;
 
