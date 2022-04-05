b30cf31ae7e9c0f008b31c847c91debc8c2cf5b8
ian-clica <ian@clica.com.ar>
2022-04-05 16:56:45 -0300

    initialize bots

--- bots/index.js
+++ bots/index.js
@@ -4,6 +4,10 @@ const BotES = require('./telegram/es.bot');
 const BotPT = require('./telegram/pt.bot');
 const BotCN = require('./telegram/cn.bot');
 const BotKR = require('./telegram/kr.bot');
+const BotIN = require('./telegram/in.bot');
+const BotIND = require('./telegram/ind.bot');
+const BotPH = require('./telegram/ph.bot');
+const BotTR = require('./telegram/tr.bot');
 
 const BotTEST = require('./telegram/test.bot');
 
@@ -19,6 +23,10 @@ class InitBots {
             new BotPT(botConfig.pt);
             new BotKR(botConfig.kr);
             new BotCN(botConfig.cn);
+            new BotIN(botConfig.in);
+            new BotIND(botConfig.ind);
+            new BotPH(botConfig.ph);
+            new BotTR(botConfig.tr);
 
         }else if(process.env.NODE_ENV == 'development'){
 
